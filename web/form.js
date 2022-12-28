bca138f3543c65730d18f4928669fe4dd3e1c93e
levietdat32 <levietdat252vip1@gmail.com>
2022-12-29 05:11:14 +0700

    web

---
+++ web/form.js
@@ -0,0 +1,18 @@
+
+function tableform() {
+    let hovaten = document.getElementById("id.hovaten")
+    let email = document.getElementById("id.email")
+    let sodienthoai = document.getElementById("id.sodienthoai")
+    let chonkhoahoc = document.getElementById("id.chonkhoahoc")
+    let loinhancuaban = document.getElementById("id.loinhancuaban")
+    let checkbox = document.getElementById("id.checkbox")
+
+    console.log(hovaten.value)
+    console.log(email.value)
+    console.log(sodienthoai.value)
+    console.log(chonkhoahoc.value)
+    console.log(loinhancuaban.value)
+    console.log(checkbox.value)
+}
+<button class="dangky" id="iddangkky" onclick="tableform()" ><b>Đăng ký</b> </button>
+
